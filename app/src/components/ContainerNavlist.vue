9a8882fffbedf368c105ba59d71e9b97a5be4b86
Giovanni <gpettinacci5@gmail.com>
2024-01-08 16:03:23 +0100

    Add NavlistButton.vue for dynamic buttons

--- app/src/components/ContainerNavlist.vue
+++ app/src/components/ContainerNavlist.vue
@@ -1,30 +1,29 @@
 <template>
     <div id="nav">
-        <ul class="navList">
-            <li> 
-                <img src="../assets/profile-user.png" class="listImg">
-                &emsp;
-                Profilo 
-            
-            </li>
-            <li> 
-                <img src="../assets/location.png" class="listImg">
-                &emsp;
-                Esplora 
-            </li>
-            <li> 
-                <img src="../assets/send.png" class="listImg">
-                &emsp;
-                Messaggi 
-            </li>
-            <li> 
-                <img src="../assets/bell.png" class="listImg">
-                &emsp;
-                Notifiche 
-            </li>
-        </ul>
+        <div class="navList">
+            <NavlistButton 
+                v-for="btn in btnList"
+                v-bind:key="btn.route"
+                v-bind:route="btn.route"
+                v-bind:img-path="btn.imgPath"
+                v-bind:title="btn.title"
+            />
+        </div>
     </div>  
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+import NavlistButton from './NavlistButton.vue';
+
+const btnList = ref([
+    {route: '/profile', imgPath: '/assets/profile-user.png', title: 'Profilo'},
+    {route: '/explore', imgPath: '/assets/location.png', title: 'Esplora'},
+    {route: '/messages', imgPath: '/assets/send.png', title: 'Messaggi'},
+    {route: '/notifications', imgPath: '/assets/bell.png', title: 'Notifiche'}
+])
+</script>
 
 <style scoped>
 /* @import '../styles/site2.css'; */
@@ -45,17 +44,26 @@
     height: 25%;
     text-align: left;
     font-size: 20px;
-    margin-top: 30px;
+    margin-top: 0;
     padding-left: 0px;
 }
 
 .navList li
 {
-    padding-left: 30px;
-    margin-bottom: 30px;
-    padding-bottom: 30px;
-    width:100%;
+    padding: 30px 30px;
+    margin: 0 0;
+    width: auto;
     border-bottom: 1px solid white;
+}
+
+.navList li:hover 
+{
+    background-color: rgb(97, 97, 97);
+}
+
+.navList li:hover .listImg
+{
+    width: 35px;
 }
 
 .listImg
---
+++ app/src/components/NavlistButton.vue
@@ -0,0 +1,21 @@
+<template>
+    <div @click="this.$router.push({ path: route })" class="page-button">
+        <img :src=imgPath class="listImg">
+        &emsp;
+        {{title}}
+    </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+defineProps({
+    route: String,
+    imgPath: String,
+    title: String
+});
+</script>
+
+<style scoped>
+@import '../styles/page_btn.css';
+</style>
